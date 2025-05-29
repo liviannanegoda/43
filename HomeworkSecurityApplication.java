14268a407b2a8efba346a83975f98ecaf0297857
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-05-29 10:38:15 +0300

    Create HomeworkSecurityApplication.java

---
+++ HomeworkSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.github.yuliyaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSecurityApplication.class, args);
+	}
+
+}
